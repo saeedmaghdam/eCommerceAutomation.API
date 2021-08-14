758ae49bdea9e9ab0229085a9b242a77ab089f6d
Saeed Mohammadi <s.mohammadi@mabnadp.com>
2021-08-14 18:59:27 +0430

    #21 Added a service to update product and destination product.

---
+++ src/eCommerceAutomation.API/Apis/V1/Models/Product/InputModels/PostUpdateTelegramSource.cs
@@ -0,0 +1,23 @@
+﻿namespace eCommerceAutomation.API.Apis.V1.Models.Product.InputModels
+{
+    public class PostUpdateTelegramSource
+    {
+        public string PriceAdjustment
+        {
+            get;
+            set;
+        }
+
+        public decimal Price
+        {
+            get;
+            set;
+        }
+
+        public bool IsDisabled
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ src/eCommerceAutomation.API/Apis/V1/Models/Product/InputModels/PostUpdateWebsiteSource.cs
@@ -0,0 +1,23 @@
+﻿namespace eCommerceAutomation.API.Apis.V1.Models.Product.InputModels
+{
+    public class PostUpdateWebsiteSource
+    {
+        public string PriceAdjustment
+        {
+            get;
+            set;
+        }
+
+        public string WholesalePriceAdjustment
+        {
+            get;
+            set;
+        }
+
+        public bool IsDisabled
+        {
+            get;
+            set;
+        }
+    }
+}
